d878f6e5ac6e039758cb862fd542166f00085e3a
jeongminmoon <answjdals15@gmail.com>
2015-11-13 21:22:58 +0900

    lecture 8
    lecture 8

---
+++ socialgraph/admin.py
@@ -0,0 +1,5 @@
+﻿from django.contrib import admin
+from socialgraph.models import UserLink
+
+# Register your models here.
+admin.site.register(UserLink)
--- startthedark/settings.py
+++ startthedark/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'events',
+    'socialgraph',
 )
 
 MIDDLEWARE_CLASSES = (
